3923cfc7c187cdc0d871cac9df993b9223b611fc
ABartolomeu-coder <124580703+ABartolomeu-coder@users.noreply.github.com>
2023-02-10 14:10:26 +0000

    Change the gif size, change the default city

--- src/app.js
+++ src/app.js
@@ -106,4 +106,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Lisbon");
